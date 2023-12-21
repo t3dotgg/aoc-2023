c00146b16e092b230b612e145a124cc87ea13ff2
Theo Browne <me@t3.gg>
2023-12-20 20:54:14 -0800

    fix template

--- template/b.ts
+++ template/b.ts
@@ -1,4 +1,4 @@
-const input: string[] = (await Bun.file("input.txt").text()).split("\n\n");
-// const input: string[] = (await Bun.file("example.txt").text()).split("\n\n");
+const input: string[] = (await Bun.file("input.txt").text()).split("\n");
+// const input: string[] = (await Bun.file("example.txt").text()).split("\n");
 
 console.log("input?", input);
